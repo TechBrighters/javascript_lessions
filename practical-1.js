1ef793e04db183c877155fd85eb9f4b35e203486
Michael <snair@rencomgroup.com>
2023-07-10 18:01:10 +0530

    For Loop practical 1

---
+++ practical-1.js
@@ -0,0 +1,36 @@
+// document.write(1)
+// document.write(2)
+// document.write(3)
+// document.write(4)
+// document.write(5)
+// document.write(6)
+// document.write(7)
+// document.write(8)
+// document.write(9)
+// document.write(10)
+//for
+//while
+//do..while
+
+//1-50 number
+
+// var n = 0;
+// var num = 1;
+// while(n < 50){
+//     document.write(num);
+//     n++;
+//     num++;
+// }
+
+// var n = 1;
+// while(n <= 50){
+//     document.write(n);
+//     n++;
+// }
+
+// for(var i=1; i < 51; i++){
+//     document.write(i,'<br>')
+// }
+
+//41-120 numbers
+//I am a Developer : 21
