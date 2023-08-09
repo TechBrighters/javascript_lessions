60dd0910d198f869956ffe16306108721fe16fcd
Mihil <Mihil>
2023-08-09 11:16:22 +0530

    Add new files

---
+++ conditional-practical-1.js
@@ -0,0 +1,55 @@
+//if...else
+//if...elseif...else
+//switch
+// let a = 10;
+// let b = 20;
+// if(a > b){
+//     alert('yes')
+// }else{
+//     alert('No')
+// }
+
+// var username = 'test@gmail.com';
+// var password = '12345';
+// var status = 1;
+// if(username == 'test@gmail.com' && password == '12345'){
+//     // if(password == '12345'){
+//         alert('yes')
+//     // }
+// }else if(status == 1){
+//         alert('active')
+// }else{
+//     alert('No')
+// }
+
+// let user = 'Manager';
+
+// switch(user){//switch('Manager')
+//         case 'Customer':
+//             alert('Customer User')
+//             break;
+
+//         case 'Admin':
+//             alert('Admin User')
+//             break;
+
+//         default:
+//             alert('It is default')
+//             break;
+// }
+
+//let x = 20;
+
+// switch(true){
+//     case x > 10:
+//         alert('First Case');
+//         break;
+//     default:
+//         alert('Default Case');
+//         break;
+
+// }
+
+//num = 50, divisible 10
+
+//category :  Electronics
---
+++ conditional-practical-2.js
@@ -0,0 +1,21 @@
+//print 1-50 numbers
+//print even
+
+for(var i=1; i < 51; i++){
+    document.write(i,'<br/>');
+    if(i == 35){
+        break;
+    }
+    
+    // if(i % 2 == 0){
+    //     document.write(i,'<br/>');
+    // }
+}
+
+//print odd
+
+//I am a Programmer; 80, 62
+// 1 => I am a Programmer
+// 2 => I am a Programmer
+// 3 => I am a Programmer
+// 62 => I am a Programmer
